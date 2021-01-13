3975b7914a828dbc8961a38a3ea48d5e27181053
silverchair <carlosdanielh@mail.com>
2021-01-12 20:50:23 -0400

    challenges finished

--- order_please.py
+++ order_please.py
@@ -1,9 +1,9 @@
-def order(cadena):
+def order(sentence):
 
-    if cadena != '':
+    if sentence != '':
         sort_list = []
-        for element in cadena.split():
-            for number in range(1, len(cadena.split())+1):
+        for number in range(1, len(sentence.split())+1):
+            for element in sentence.split():
                 if str(number) in element:
                     sort_list.append(element)
                     break
@@ -12,4 +12,24 @@ def order(cadena):
         return ''
 
 
-print(order("is2 Thi1s T4est 3a"))
+def order1(sentence):
+    return " ".join(sorted(sentence.split(),
+                    key=lambda x: int(filter(str.isdigit, x))))
+
+
+def order2(sentence):
+    return " ".join(sorted(sentence.split(), key=min))
+
+
+def order3(sentence):
+    res = sentence.split()
+    res.sort(key=lambda s: filter(str.isdigit, s))
+    return ' '.join(res)
+
+
+def order4(words):
+  return ' '.join(sorted(words.split(), key=lambda w:sorted(w)))
+
+
+
+print(order4("4of Fo1r pe6ople g3ood th5e the2"))
